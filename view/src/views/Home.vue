b5a73177e3b2f706f629abb27286996ca1dd28cd
mrjim06 <labsuekthawichat06@gmail.com>
2019-06-27 18:39:38 +0900

    edut label detect page (front-end)

--- view/src/views/Home.vue
+++ view/src/views/Home.vue
@@ -13,11 +13,22 @@
         </div>
       </form>
       <div class="row" v-if="img">
-        <div class="col s12 m5">
+        <div class="col s6">
+          <div id="detect-btn">
+              <a class="waves-effect waves-light btn">
+                <i class="material-icons right">photo_library</i>
+                execute
+              </a>
+          </div>
           <div class="card-panel">
-            <img v-if="img" :src="img" /> 
+            <img :src="img" id="preview">
           </div>
         </div>
+
+        <div class="col s6">
+          
+        </div>
+
       </div>
     </div>
   </div>
@@ -41,6 +52,15 @@ export default class Home extends Vue {
 
 <style lang="scss">
   #blog {
-    margin-top: 40px
+    margin-top: 40px;
+  }
+
+  #preview {
+    width: 100%;
+    height: 100%;
+  }
+
+  #detect-btn {
+    margin-bottom: 2%;
   }
 </style>
